48ee8e8b79265f31c0b2ee9f649faf70eb6ea7d0
DennisYako <dennisyakovlev40@gmail.com>
2020-05-07 20:54:48 -0400

    added "getGamePixelPositions" and "getPixelColor" to object.Object

--- Project/src/objects/Object.java
+++ Project/src/objects/Object.java
@@ -18,8 +18,10 @@ public class Object {
 	private int x, y;
 	private String fileName, objectName;
 	private BufferedImage image;
-	private ArrayList<Color> pixelsTemp = new ArrayList<>();
+	private ArrayList<Color> colorsTemp = new ArrayList<>();
 	private ArrayList<int[]> positionsTemp = new ArrayList<>();
+	private int[][] temp = new int[6912][2];
+	private Color[] temp2 = new Color[6912];
 	
 	public Object(int x, int y, String fileName, String objectName) {
 		
@@ -41,22 +43,7 @@ public class Object {
 			for (int j = 0; j < Constant.imageWidth; j++) {
 				
 				java.awt.Color pixelColor = new java.awt.Color(image.getRGB(j, i));
-				
-				if (colorToRemove == null) {
-					
-					pixelsTemp.add(Color.rgb(pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue()));
-					positionsTemp.add(new int[] {j, i});
-					
-				} else {
-					
-					if (Math.abs(pixelColor.getGreen() - pixelColor.getGreen()) > 2 && Math.abs(pixelColor.getRed() - colorToRemove.getRed()) > 2 && Math.abs(pixelColor.getBlue() - colorToRemove.getBlue()) > 2) {
-						pixelsTemp.add(Color.rgb(pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue()));
-						positionsTemp.add(new int[] {j, i});
-					}
-					
-				}
-				
-				pixelsTemp.add(Color.rgb(pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue()));
+				colorsTemp.add(Color.rgb(pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue()));
 				positionsTemp.add(new int[] {j, i});
 				
 			}
@@ -65,7 +52,7 @@ public class Object {
 		
 	}
 	
-	public void getImage() {
+	public void setImage() {
 		
 		String classPath;
 		FileInputStream inputStream;
@@ -91,7 +78,56 @@ public class Object {
 	
 	public void resize() {
 		
+	}		
+	
+	public int[][] getGamePixelPositions() {
+
+		if (positionsTemp.size() != 6912) {
+			throw new ImageException("Expected array length of 6912, instead got " + positionsTemp.size());
+		}
 		
+		for (int i = 0; i < positionsTemp.size(); i++) {
+			temp[i] = new int[] {positionsTemp.get(i)[0] * 5, positionsTemp.get(i)[1] * 5};
+		}	
 		
+		return temp;
 	}
+	
+	public Color[] getPixelColor() {
+		
+		if (colorsTemp.size() != 6912) {
+			throw new ImageException("Expected array length of 6912, instead got " + colorsTemp.size());
+		}
+		
+		for (int i = 0; i < colorsTemp.size(); i++) {
+			temp2[i] = colorsTemp.get(i);
+		}
+		
+		return temp2;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+useful dont touch
+
+					
+if (Math.abs(pixelColor.getGreen() - pixelColor.getGreen()) > 2 && Math.abs(pixelColor.getRed() - colorToRemove.getRed()) > 2 && Math.abs(pixelColor.getBlue() - colorToRemove.getBlue()) > 2) {
+	pixelsTemp.add(Color.rgb(pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue()));
+	positionsTemp.add(new int[] {j, i});
+}
+					
+				
+
+*/
